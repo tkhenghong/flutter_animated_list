edc25e963699e51f7d1eed3a8fe44723d381a4b4
Teoh Kheng Hong <tkhenghong@gmail.com>
2019-09-26 08:16:57 +0800

    Initial commit

---
+++ android/app/src/main/kotlin/test/list/animated/flutter/com/flutter_animated_list/MainActivity.kt
@@ -0,0 +1,13 @@
+package test.list.animated.flutter.com.flutter_animated_list
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
